3b3c12903eb83bc56ab36945a3ad18d2dbe6eaf4
imAnkurKumar <imankur.ak@gmail.com>
2023-02-17 08:32:54 +0530

    adding partial and template

--- expressJs/express.js
+++ expressJs/express.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const path = require("path");
+
 const app = express();
 
-const pathdir = path.join(__dirname, "public");
+// const pathdir = path.join(__dirname, "public");
 // console.log(pathdir);
+// app.use(express.static(pathdir));
 
-app.use(express.static(pathdir));
+app.set("view engine", "ejs");
 
 app.get("/", (req, resp) => {
-  resp.sendFile(`${pathdir}/index.html`);
+  // resp.sendFile(`${pathdir}/index.html`);
+  resp.render("index");
 });
 
 app.get("/home", (req, resp) => {
-  resp.sendFile(`${pathdir}/home.html`);
+  // resp.sendFile(`${pathdir}/home.html`);
+  resp.render("home");
 });
 
 app.listen(4000);
